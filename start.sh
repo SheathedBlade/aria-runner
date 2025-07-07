0a16042f219639b5b350d11a2027048d84ef5fcd
SheathedBlade <warlordpluto@gmail.com>
2025-07-07 01:04:37 -0400

    fixed start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,10 @@
 REPO=$REPO
 ACCESS_TOKEN=$TOKEN
 
-REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output
+echo "Repository: ${REPO}"
+echo "Token: ${ACCESS_TOKEN}"
+
+REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
@@ -16,3 +19,5 @@ cleanup() {
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
