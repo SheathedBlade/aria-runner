59cc12d19d360e38b01933ec74ab672963c48a05
SheathedBlade <warlordpluto@gmail.com>
2025-07-07 01:08:14 -0400

    removes echo statements

--- start.sh
+++ start.sh
@@ -3,9 +3,6 @@
 REPO=$REPO
 ACCESS_TOKEN=$TOKEN
 
-echo "Repository: ${REPO}"
-echo "Token: ${ACCESS_TOKEN}"
-
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
